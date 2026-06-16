57793a3e84e39ddb209e682aed4f5049b9805fbc
Fallet666 <fallet666@gmail.com>
2026-06-16 15:25:09 +0300

    Rename fan-control cask to vent, update URLs for repo rename

---
+++ Casks/vent.rb
@@ -0,0 +1,35 @@
+cask "vent" do
+  version "1.1.7"
+  sha256 "a7556948bf20043b4aa27c5f23340ff257fe4d83c7f0306322472a5ab0f8045c"
+
+  url "https://github.com/Fallet666/vent/releases/download/v#{version}/Vent-#{version}.dmg"
+  name "Vent"
+  desc "macOS fan speed controller with Apple Silicon support"
+  homepage "https://github.com/Fallet666/vent"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Vent.app"
+
+  uninstall quit: "dev.borninvoid.macfancontrol"
+
+  zap trash: [
+    "~/Library/Preferences/dev.borninvoid.macfancontrol.plist",
+  ]
+
+  caveats <<~EOS
+    Vent requires a privileged helper daemon for fan control.
+
+    Install it by launching the app and clicking "Install / Update"
+    in the menu-bar popover settings.
+
+    Or install manually:
+
+        sudo /Applications/Vent.app/Contents/Resources/ventd --install
+  EOS
+end
